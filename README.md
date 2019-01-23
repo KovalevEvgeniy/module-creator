412d06918450fa513ae71b9e1d4a0d4f3f036883
White <ametist16@gmail.com>
2019-01-23 14:53:00 +0300

    git init

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# ModuleCreator
+# ModuleCreator 
+#### version 1.1
+
+## Usage
+
+### Create module
+``` js
+CreateModule({
+	name: 'Name',
+	data: {},
+	options: {},
+	hooks: {
+		beforeCreate: function () {},
+		create: function () {
+			this._create()
+		},
+		bindEvent: function () {
+			$(el).on(getEventName('click', this.hash), this._onClick)
+		},
+		afterCreate: function () {}
+	},
+	privateMethods: {
+		_create: function () {},
+		_onClick: function (e) {},
+		_examplePrivateMethod: function (e) {}
+	},
+	publicMethods: {
+		examplePublicMethod: function (e) {
+			this.private._examplePrivateMethod()
+		}
+	}
+});
+```
+### Init module
+``` js
+$(function () {
+	$.name([options])
+})
+```
+or
+``` js
+$(function () {
+	$(selector).name([options])
+})
+```
+### Call methods
+``` js
+$.name('examplePublicMethod', [arguments])
+```
+
+### Patch Notes
+#### v 1.1 
+Add global storage
+
+#### v 1.0
+Release
---
+++ example/example.js
@@ -0,0 +1,54 @@
+/**
+* @ModuleCreator version 1.0
+* @module ModuleName
+* @plugin moduleName
+* @example $.moduleName(object)
+* @author Kovalev Evgeniy
+**/
+
+CreateModule({
+	name: 'ModuleName',
+	data: {},
+	options: {
+		exampleOption: false,
+	},
+	hooks: {
+		beforeCreate: function () {},
+		create: function () {
+			this._create()
+		},
+		bindEvent: function () {
+			$(this.element).on(getEventName('click', this.hash), this._onClick)
+		},
+		afterCreate: function () {},
+		customHook: function () {
+			console.log('custom code')
+		}
+	},
+	privateMethods: {
+		_create: function () {
+			if (this.options.exampleOption) {
+				console.log('base initing')
+			}
+			$(el).trigger('create', {})
+		},
+		_onClick: function (e) {
+			// this - инст модуля
+			var element = $(e.currentTarget)
+			console.log('anithing code')
+			this.hooks('customHook')
+		},
+		_examplePrivateMethod: function () {
+			var element = this.element
+			console.log('private code');
+		}
+	},
+	publicMethods: {
+		examplePublicMethod: function (e) {
+			// this.inst - инст модуля
+			// this.private - доступ к приватным методам из публичной области
+			console.log('public code')
+			this.private._examplePrivateMethod()
+		}
+	}
+});
