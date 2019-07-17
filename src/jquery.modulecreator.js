350fc112d71ac94b4e48378ba5c996df766723ec
Evgeniy <ametist16@gmail.com>
2019-07-17 13:18:39 +0300

    fix bugs

--- src/jquery.modulecreator.js
+++ src/jquery.modulecreator.js
@@ -29,9 +29,9 @@
 							const inst = new Module(item, options);
 							inst.list[inst.hash] = inst;
 						} else {
-							try {
+							if (item[lib][options] && typeof item[lib][options] === 'function') {
 								result = (item[lib][options].apply(item[lib], args) || selector);
-							} catch (error) {
+							} else {
 								throw new Error('Method "' + options + '" is not defined in the "' + lib + '" module');
 							}
 						}
@@ -209,7 +209,11 @@
 			}
 
 			addHooks (inst) {
-				const hooks = Tools.extend({}, (props.hooks || {}), (this.options.hooks || {}));
+				const hooks = Tools.extend({
+					beforeCreate: function () {},
+					bindEvent: function () {},
+					afterCreate: function () {}
+				}, (props.hooks || {}), (this.options.hooks || {}));
 				Tools.haveFunctions(hooks);
 
 				Object.defineProperty(inst, 'hook', {
@@ -275,7 +279,7 @@
 				Object.defineProperty(inst, 'super', {
 					get: () => (function (name, argument, ...args) {
 						try {
-							if (typeof props.parents[name] === 'object' && props.parents[name][argument]) {
+							if (props.parents && typeof props.parents[name] === 'object' && props.parents[name][argument]) {
 								return props.parents[name][argument].apply(this, args);
 							}
 							return inst.__proto__[name].apply(this, [argument, ...args]);
