b8956ffa37a7403f29a3638c2bde48a5089f0198
Evgeniy <evgenij.kovalev@promo.ru>
2020-02-10 17:01:14 +0300

    fix bug with for of in ie 11

--- example/tests.js
+++ example/tests.js
@@ -13,12 +13,15 @@ const Test = {
 		error: 'color: #d62020',
 		life: 'color: #777'
 	},
-	getRow () {
-		const errorPath = new Error().stack.split('\n')[3].split(':');
-
-		return errorPath[errorPath.length - 2]
+	getRow: function () {
+		if (new Error().stack) {
+			const errorPath = new Error().stack.split('\n')[3].split(':');
+			return errorPath[errorPath.length - 2]
+		} else {
+			return 0
+		}
 	},
-	log (message, type) {
+	log: function (message, type) {
 		const row = ' :(' + this.getRow() + ')';
 
 		if (type === 'success') {
@@ -57,7 +60,7 @@ $(function () {
 				return false
 			},
 			_testExtend: function () {
-				Test.log(`Parent method called for name TestParent`, 'life');
+				Test.log('Parent method called for name TestParent', 'life');
 				return true
 			}
 		}
@@ -82,7 +85,7 @@ $(function () {
 				return true
 			},
 			_testExtend: function (isSuper) {
-				Test.log(`Parent method called for ${isSuper ? 'super' : 'name'} TestParent2`, 'life');
+				Test.log('Parent method called for ' + (isSuper ? 'super' : 'name') + ' TestParent2', 'life');
 				return true
 			}
 		}
@@ -97,13 +100,13 @@ $(function () {
 			watchingData: 'old'
 		},
 		watch: {
-			watchingData (oldValue, newValue) {
+			watchingData: function (oldValue, newValue) {
 				window.watchingData = true;
 
 				if (oldValue === 'old' && newValue === 'new') {
-					Test.log(`The observing data method works correctly`, 'success');
+					Test.log('The observing data method works correctly', 'success');
 				} else {
-					Test.log(`The observing data method does not work correctly`, 'error');
+					Test.log('The observing data method does not work correctly', 'error');
 				}
 			}
 		},
@@ -113,22 +116,22 @@ $(function () {
 		},
 		hooks: {
 			beforeCreate: function () {
-				Test.log(`Life cycle: beforeCreate`, 'life');
+				Test.log('Life cycle: beforeCreate', 'life');
 			},
 			create: function () {
-				Test.log(`Life cycle: create`, 'life');
+				Test.log('Life cycle: create', 'life');
 				this._init()
 			},
 			bindEvent: function () {
-				Test.log(`Life cycle: bindEvent`, 'life');
+				Test.log('Life cycle: bindEvent', 'life');
 				$(this.element).on(this._getEventName('click', this.hash), this._testClick);
 				$(this.element).trigger('click');
 			},
 			afterCreate: function () {
-				Test.log(`Life cycle: afterCreate`, 'life');
+				Test.log('Life cycle: afterCreate', 'life');
 			},
 			customHook: function () {
-				Test.log(`Hook is working`, 'success');
+				Test.log('Hook is working', 'success');
 			}
 		},
 		privateMethods: {
@@ -161,7 +164,7 @@ $(function () {
 				try {
 					this.hook('customHook');
 				} catch (err) {
-					Test.log(`Hook dont working`, 'error');
+					Test.log('Hook dont working', 'error');
 				}
 			},
 			_testEditable: function (name) {
@@ -182,15 +185,15 @@ $(function () {
 				}
 
 				if (isEditableObject) {
-					Test.log(`Object "${name}" is editable`, 'error');
+					Test.log('Object ' + name + ' is editable', 'error');
 				} else {
-					Test.log(`Object "${name}" is not editable`, 'success');
+					Test.log('Object ' + name + ' is not editable', 'success');
 				}
 
 				if (isRewritableProp) {
-					Test.log(`Object "${name}" properties rewritable`, 'success');
+					Test.log('Object ' + name + ' properties rewritable', 'success');
 				} else {
-					Test.log(`Object "${name}" is not properties rewritable`, 'error');
+					Test.log('Object ' + name + ' is not properties rewritable', 'error');
 				}
 			},
 			_testWatchCalls: function () {
@@ -198,88 +201,88 @@ $(function () {
 				this.data.watchingData = 'new';
 
 				if (window.watchingData !== true) {
-					Test.log(`Data is not watching`, 'error');
+					Test.log('Data is not watching', 'error');
 				}
 
 				this._set('newWatchingData', 'old', function (oldValue, newValue) {
 					window.newWatchingData = true;
 
 					if (oldValue === 'old' && newValue === 'newest') {
-						Test.log(`The observing new data method works correctly`, 'success');
+						Test.log('The observing new data method works correctly', 'success');
 					} else {
-						Test.log(`The observing new data method does not work correctly`, 'error');
+						Test.log('The observing new data method does not work correctly', 'error');
 					}
 				});
 
 				this.data.newWatchingData = 'newest';
 
 				if (window.newWatchingData !== true) {
-					Test.log(`New data is not watching`, 'error');
+					Test.log('New data is not watching', 'error');
 				}
 			},
 			_testClick: function (e) {
 				if (e.type === 'click') {
-					Test.log(`Event is available`, 'success');
+					Test.log('Event is available', 'success');
 				}
 			},
 			_testExtends: function () {
 				console.log('extends:');
 				if (this.options.parentOption && this.options.parentOption_2) {
-					Test.log(`Parent options is available`, 'success');
+					Test.log('Parent options is available', 'success');
 				} else {
-					Test.log(`Parent options is not available`, 'error');
+					Test.log('Parent options is not available', 'error');
 				}
 
 				try {
 					this._parentMethod();
 					this._parentMethod_2();
-					Test.log(`Parent methods is available`, 'success');
+					Test.log('Parent methods is available', 'success');
 				} catch (err) {
-					Test.log(`Parent methods is not available`, 'error');
+					Test.log('Parent methods is not available', 'error');
 				}
 
 				if (this.hook('parentHook') && this.hook('parentHook_2')) {
-					Test.log(`Parent hooks is available`, 'success');
+					Test.log('Parent hooks is available', 'success');
 				} else {
-					Test.log(`Parent hooks is not available`, 'error');
+					Test.log('Parent hooks is not available', 'error');
 				}
 
 				if (this.super('_testExtends') && this.super('_testExtends_2')) {
-					Test.log(`Parent methods to be called with super`, 'success');
+					Test.log('Parent methods to be called with super', 'success');
 				} else {
-					Test.log(`Parent methods cannot be called with super`, 'error');
+					Test.log('Parent methods cannot be called with super', 'error');
 				}
 			},
-			_testInstanceOptions () {
+			_testInstanceOptions: function () {
 				console.log('instance properties:');
 				if (this.options.option1) {
-					Test.log(`Default options is available`, 'success');
+					Test.log('Default options is available', 'success');
 				} else {
-					Test.log(`Default options is not available`, 'error');
+					Test.log('Default options is not available', 'error');
 				}
 				if (this.options.option2) {
-					Test.log(`Inctance initing options is available`, 'success');
+					Test.log('Inctance initing options is available', 'success');
 				} else {
-					Test.log(`Inctance initing options is not available`, 'error');
+					Test.log('Inctance initing options is not available', 'error');
 				}
 				if (this.data.data1) {
-					Test.log(`Default data is available`, 'success');
+					Test.log('Default data is available', 'success');
 				} else {
-					Test.log(`Default data is not available`, 'error');
+					Test.log('Default data is not available', 'error');
 				}
 				if (this.data.data2) {
-					Test.log(`Inctance initing data is available`, 'success');
+					Test.log('Inctance initing data is available', 'success');
 				} else {
-					Test.log(`Inctance initing data is not available`, 'error');
+					Test.log('Inctance initing data is not available', 'error');
 				}
 
 				this._defaultMethod();
 				this.element.testName('default');
 			},
-			_defaultMethod () {
-				Test.log(`Inctance initing private method is not available`, 'success');
+			_defaultMethod: function () {
+				Test.log('Inctance initing private method is not available', 'success');
 			},
-			_testGlobalMethods () {
+			_testGlobalMethods: function () {
 				console.log('Global methods:');
 				// Test extend
 				const obj1 = {b: false, d:false, c: true};
@@ -289,24 +292,24 @@ $(function () {
 					{ a: true, d: true}
 				);
 				if (obj1 === obj2 && obj2.a && obj2.b && obj2.c && obj2.d) {
-					Test.log(`Method "_extend" is working!`, 'success');
+					Test.log('Method "_extend" is working!', 'success');
 				} else {
-					Test.log(`Method "_extend" is not working!`, 'error');
+					Test.log('Method "_extend" is not working!', 'error');
 				}
 
 				// Test deepCopy
 				const obj3 = this._deepCopy(obj1);
 				if (typeof obj3 === 'object' && obj3 !== obj1) {
-					Test.log(`Method "_deepCopy" is working!`, 'success');
+					Test.log('Method "_deepCopy" is working!', 'success');
 				} else {
-					Test.log(`Method "_deepCopy" is not working!`, 'error');
+					Test.log('Method "_deepCopy" is not working!', 'error');
 				}
 
 				// Check _isMobile method available
 				if (typeof this._isMobile === 'function') {
-					Test.log(`Method "_isMobile" is available!`, 'success');
+					Test.log('Method "_isMobile" is available!', 'success');
 				} else {
-					Test.log(`Method "_isMobile" is not available!`, 'error');
+					Test.log('Method "_isMobile" is not available!', 'error');
 				}
 			}
 		},
@@ -315,21 +318,21 @@ $(function () {
 				try {
 					this.private = 'test string';
 				} catch (err) {
-					Test.log(`Object "private" is not editable`, 'success');
+					Test.log('Object "private" is not editable', 'success');
 				}
 
 				if (typeof this.private === 'object') {
 					if (this.private && this.private._tests) {
-						Test.log(`Private methods are available from the public method`, 'success');
+						Test.log('Private methods are available from the public method', 'success');
 					} else {
-						Test.log(`Private methods are not available from the public method`, 'error');
+						Test.log('Private methods are not available from the public method', 'error');
 					}
 				} else {
-					Test.log(`Object "private" is editable`, 'error');
+					Test.log('Object "private" is editable', 'error');
 				}
 			},
 			default: function () {
-				Test.log(`Inctance initing public method is not available`, 'success');
+				Test.log('Inctance initing public method is not available', 'success');
 			}
 		}
 	});
@@ -341,7 +344,7 @@ $(function () {
 		options: {},
 		hooks: {
 			create: function () {
-				Test.log(`Life cycle: create`, 'life');
+				Test.log('Life cycle: create', 'life');
 				this._testExtendsChild();
 			},
 			bindEvent: function () {
@@ -353,9 +356,9 @@ $(function () {
 				this.super('_testExtend', true);
 
 				if (this.super('testParent', '_testExtend') && this.super('testParent2', '_testExtend')) {
-					Test.log(`Parent hight lvl methods to be called with super`, 'success');
+					Test.log('Parent hight lvl methods to be called with super', 'success');
 				} else {
-					Test.log(`Parent hight lvl methods cannot be called with super`, 'error');
+					Test.log('Parent hight lvl methods cannot be called with super', 'error');
 				}
 				return true
 			}
@@ -363,9 +366,9 @@ $(function () {
 		publicMethods: {
 			testExtendsChild: function () {
 				if (this.private._testHigthLvlExtend()) {
-					Test.log(`Parent hight lvl methods is available`, 'success');
+					Test.log('Parent hight lvl methods is available', 'success');
 				} else {
-					Test.log(`Parent hight lvl methods are not available`, 'error');
+					Test.log('Parent hight lvl methods are not available', 'error');
 				}
 			}
 		}
@@ -385,12 +388,12 @@ $(function() {
 		},
 		privateMethods: {
 			_defaultMethod: function () {
-				Test.log(`Inctance initing private method is available`, 'error');
+				Test.log('Inctance initing private method is available', 'error');
 			}
 		},
 		publicMethods: {
 			default: function () {
-				Test.log(`Inctance initing public method is available`, 'error');
+				Test.log('Inctance initing public method is available', 'error');
 			}
 		}
 	});
