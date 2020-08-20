c21a4d46da9d23adb8ea4d8b04ad6ccb84600b1e
Evgeniy Igorevich <ametist16@gmail.com>
2020-08-20 14:39:30 +0300

    hot fix

--- dist/jquery.modulecreator.js
+++ dist/jquery.modulecreator.js
@@ -218,13 +218,14 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
               return inst.data[name];
             },
             set: function set(newValue) {
-              if (callback && typeof callback === 'function') {
-                callback.call(inst.inst, inst.data[name], newValue);
-              } else if (inst.watch[name] && typeof inst.watch[name] === 'function') {
-                inst.watch[name].call(inst.inst, inst.data[name], newValue);
-              }
-
+              var oldValue = inst.data[name];
               inst.data[name] = newValue;
+
+              if (callback && typeof callback === 'function') {
+                callback.call(inst.inst, oldValue, inst.data[name]);
+              } else if (inst.watch[name] && typeof inst.watch[name] === 'function') {
+                inst.watch[name].call(inst.inst, oldValue, inst.data[name]);
+              }
             }
           });
           inst.data[name] = value;
